397716e232458adfbba8a7ed61016814dad645b4
kingsolomon100 <elohosoli@gmail.com>
2026-01-12 13:48:27 -0800

    first commit

--- route/route.py
+++ route/route.py
@@ -26,7 +26,7 @@ async def postman(blog: Blogmodel, response: Response ):
         return {"error": "Categories is required"}
     
     if not blog_dict["category"]:
-        response.status_code = status.HTTP_400_BAD_REQUEST
+        response.status_code = status.HTTP_400_BAD_REQUEST 
         return {"error": "Category is required"}
     
     if not blog_dict["title"]:
